55b6ace16a3f82c3e55a91ec7dfad7da045d03f3
yr5913 <yr5913@rit.edu>
2024-03-18 19:41:46 -0400

    Added dynmaic lists using props

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,22 @@
 import List from "./List.jsx";
 function App() {
+  const fruits = [
+    { id: 1, name: "apple", calories: 95 },
+    { id: 2, name: "banana", calories: 105 },
+    { id: 3, name: "orange", calories: 45 },
+    { id: 4, name: "strawberry", calories: 56 },
+  ];
+
+  const veggies = [
+    { id: 6, name: "potatoes", calories: 95 },
+    { id: 7, name: "celery", calories: 15 },
+    { id: 8, name: "carrots", calories: 95 },
+    { id: 9, name: "corn", calories: 63 },
+  ];
   return (
     <>
-      <List></List>
+      <List items={fruits} category="Fruits"></List>
+      <List items={veggies} category="Vegetables"></List>
     </>
   );
 }
--- src/List.jsx
+++ src/List.jsx
@@ -1,18 +1,17 @@
-function List() {
-  const fruits = [
-    { id: 1, name: "apple", calories: 95 },
-    { id: 2, name: "banana", calories: 105 },
-    { id: 3, name: "orange", calories: 45 },
-    { id: 4, name: "strawberry", calories: 56 },
-  ];
-  fruits.sort((a, b) => a.calories - b.calories);
-  const lowCalFruits = fruits.filter((fruit) => fruit.calories < 100);
-  const highCalFruits = fruits.filter((fruit) => fruit.calories >= 100);
-  const listItems = highCalFruits.map((fruit) => (
-    <li key={fruit.id}>
-      {fruit.name}:&nbsp;<b>{fruit.calories}</b>
+function List(props) {
+  const items = props.items;
+  const category = props.category;
+
+  const listItems = items.map((item) => (
+    <li key={item.id}>
+      {item.name}:&nbsp;<b>{item.calories}</b>
     </li>
   ));
-  return <ol>{listItems}</ol>;
+  return (
+    <>
+      <h3 className="list-category">{category}</h3>
+      <ol className="list-items">{listItems}</ol>
+    </>
+  );
 }
 export default List;
