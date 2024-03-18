99ead2782e86f1d7e5aa65b8bff899005e49d2c1
yr5913 <yr5913@rit.edu>
2024-03-18 19:28:39 -0400

    Added filters to display low calorie friuts

--- src/List.jsx
+++ src/List.jsx
@@ -1,7 +1,18 @@
 function List() {
-  const fruits = ["apple", "banana", "orange", "strawberry"];
-  fruits.sort();
-  const listItems = fruits.map((fruit) => <li>{fruit}</li>);
+  const fruits = [
+    { id: 1, name: "apple", calories: 95 },
+    { id: 2, name: "banana", calories: 105 },
+    { id: 3, name: "orange", calories: 45 },
+    { id: 4, name: "strawberry", calories: 56 },
+  ];
+  fruits.sort((a, b) => a.calories - b.calories);
+  const lowCalFruits = fruits.filter((fruit) => fruit.calories < 100);
+  const highCalFruits = fruits.filter((fruit) => fruit.calories >= 100);
+  const listItems = highCalFruits.map((fruit) => (
+    <li key={fruit.id}>
+      {fruit.name}:&nbsp;<b>{fruit.calories}</b>
+    </li>
+  ));
   return <ol>{listItems}</ol>;
 }
 export default List;
