26c7f7791fe057415ed6bd36ef63fdec493806cb
yr5913 <yr5913@rit.edu>
2024-03-18 20:17:33 -0400

    on double click event

--- src/Button.jsx
+++ src/Button.jsx
@@ -2,8 +2,9 @@ function Button() {
   let count = 0;
   const handleClick2 = (e) => {
     console.log(e);
+    e.target.textContent = "Ouch";
   };
-  return <button onClick={(e) => handleClick2(e)}>Click me</button>;
+  return <button onDoubleClick={(e) => handleClick2(e)}>Click me</button>;
 }
 
 export default Button;
