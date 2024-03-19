8bcf21a21e761f2011b99bdd36975289cdd829c2
yr5913 <yr5913@rit.edu>
2024-03-19 19:14:14 -0400

    made changes to run code when the component mount and when dependencies change

--- src/MyComponentUseEffect.jsx
+++ src/MyComponentUseEffect.jsx
@@ -1,18 +1,29 @@
 import React, { useEffect, useState } from "react";
 function MyComponentUseEffect() {
   const [count, setCount] = useState(0);
+  const [color, setColor] = useState("green");
 
   useEffect(() => {
-    document.title = `My Counter Program`;
-  }, []);
+    document.title = `Count:${count} ${color}`;
+  }, [count]);
 
   function addCount() {
     setCount((c) => c + 1);
   }
+
+  function decreaseCount() {
+    setCount((c) => c - 1);
+  }
+  function changeColor() {
+    setColor((c) => (c === "green" ? "red" : "green"));
+  }
   return (
     <>
-      <p>Count: {count}</p>
+      <p style={{ color: color }}>Count: {count}</p>
       <button onClick={addCount}>Add</button>
+      <button onClick={decreaseCount}>Decrease</button>
+      <br></br>
+      <button onClick={changeColor}>Change Color</button>
     </>
   );
 }
